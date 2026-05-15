3f143d4c7e94993c85056d13f3d8047eedfc1c7f
jangthesuyog300-gitch <jangthesuyog300gmain.com>
2026-05-15 11:39:32 +0530

    frontend and backend

--- backend/src/db/migrations/005_add_store_category.sql
+++
@@ -1,5 +0,0 @@
--- Add category column to stores table
-ALTER TABLE stores ADD COLUMN category VARCHAR(50);
-
--- Update existing stores with default categories
-UPDATE stores SET category = 'Grocery' WHERE category IS NULL;
